3603780352c27230394634ad7b818c0cdd32de1a
domchang1 <dominicchang08@gmail.com>
2022-05-24 22:32:15 -0700

    played around with text, nav bar, images, buttons

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./images/ceng.png')}/>
+      <Text style={styles.title}>COMPUTER ENGINEERS OF THE NEXT GENERATION</Text>
+      <StatusBar style="auto" />
+      <Button title="Solid" />
+
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 64,
+    textAlign: 'center',
+  }
 });
