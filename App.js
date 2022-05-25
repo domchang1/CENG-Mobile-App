c4e0abc6046a864821ffae1fcd3dce8a7e6ad0da
domchang1 <dominicchang08@gmail.com>
2022-05-24 23:10:48 -0700

    working w linking

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Button, Text, View, Image } from 'react-native';
+import React, { useCallback } from 'react';
+import { StyleSheet, Button, Text, View, Image, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const OpenURLButton = ({ url, children }) => { //https://reactnative.dev/docs/linking
+  const handlePress = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+
+  return <Button title={children} onPress={handlePress} />;
+};
 
 const Tab = createBottomTabNavigator();
 function HomeScreen() {
@@ -11,7 +29,7 @@ function HomeScreen() {
       <Image source={require('./images/ceng.png')}/>
       <Text style={styles.title}>COMPUTER ENGINEERS OF THE NEXT GENERATION</Text>
       <StatusBar style="auto" />
-      <Button title="Solid" />
+      <OpenURLButton url={'https://www.cengclass.org/'}>Our Website</OpenURLButton>
 
     </View>
   );
